0d487c3f01f1ca728bf4f0406ce6ce127ae0c4b1
RoberGF <rgfrasta@gmail.com>
2026-06-01 16:58:25 +0200

    First commit, health HTTP/GET

---
+++ src/functions/health.ts
@@ -0,0 +1,19 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function health(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    return {
+        status: 200,
+        jsonBody: {
+            status: "ok",
+            service: "order management api"
+        }
+    };
+};
+
+app.http('health', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: health
+});
