a989fe082efb58c44a3f3b746ddfa019101ad39c
Roberto Gomez Fabrega <rgfrasta@gmail.com>
2026-06-02 11:00:47 +0200

    Feat  create order use case (#4)
    create order entity and repository

---
+++ src/infrastructure/repositories/in-memory-order.repository.ts
@@ -0,0 +1,10 @@
+import { Order } from "../../domain/entities/order.entity";
+import { OrderRepository } from "../../domain/ports/order.repository";
+
+const orders = new Map<string, Order>();
+
+export const inMemoryOrderRepository: OrderRepository = {
+  async save(order: Order): Promise<void> {
+    orders.set(order.id, order);
+  },
+};
